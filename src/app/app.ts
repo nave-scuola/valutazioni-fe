a1826131ba91d28cfb072e025f8b334e2560d4f5
carlotta-siddi <carlotta.siddi97@gmail.com>
2026-05-27 12:03:06 +0200

    Aggiunta visualizzazione card

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Card } from './shared/components/card/card';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
